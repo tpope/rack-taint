9c86fddb04bf3052eeb0f577687d96ea461d9f75
Tim Pope <code@tpope.net>
2012-03-15 13:22:16 -0400

    Don't assume presence of Rack::MethodOverride
    https://github.com/rails/rails/commit/c84d3276def77889fa7b5833ebc51919b349c6ae

--- lib/rack/taint/railtie.rb
+++ lib/rack/taint/railtie.rb
@@ -2,7 +2,7 @@ module Rack
   class Taint
     class Railtie < Rails::Railtie
       initializer 'rack.taint.configure_rails_initialization' do |app|
-        app.middleware.insert_before(Rack::MethodOverride, Rack::Taint)
+        app.middleware.insert_before(Rack::Runtime, Rack::Taint)
       end
     end
   end
